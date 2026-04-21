a22ed280925ca68cb9549ae998f9195b88e13e6e
jerry <dk.illidan@gmail.com>
2026-04-21 17:18:33 +0800

    Enable VCT by default and export direct trace

--- pygomoku/config.py
+++ pygomoku/config.py
@@ -32,8 +32,8 @@ class RuntimeOptions:
     nonroot_vcf: bool
     static_board: bool
     dynamic_board_margin: int
-    compute_vct: bool = False
-    root_vct_depth: int = 4
+    compute_vct: bool = True
+    root_vct_depth: int = 8
 
 
 @dataclass(frozen=True)
